2c120c9c4b5fc6e9b0257ac938f30cd5aaf4e88e
Ronie Peterson de O. A. dos Santos <Ronie.Santos@EDUC.NET>
2022-04-27 08:49:54 -0300

    adicionando projetos carros e crud

---
+++ carros/models/CarroDB.js
@@ -0,0 +1,147 @@
+const mysql = require("mysql");
+
+class CarroDB {
+  static connect() {
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user:  'root',
+      password: '',
+      database: 'nodejs_crud'
+    });
+    
+    connection.connect();
+    
+    return connection;
+  }
+
+  static getCarros(callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `SELECT * FROM carro`;
+    const query = connection.query(query, (error, results, fields) => {
+      if (error) throw error;
+
+      callback(results);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static getCarrosByTipo(tipo, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `SELECT id, nome, tipo FROM carro WHERE tipo = '${tipo}'`;
+    const query = connection.query(query, (error, results, fields) => {
+      if (error) throw error;
+
+      callback(results);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static getCarroById(id, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `SELECT * FROM carro WHERE id = ?`;
+
+    const query = connection.query(sql, id, (error, results, fields) => {
+      if (error) throw error;
+
+      if(results.length == 0) {
+        console.log("Nenhum carro encontrado.");
+        return;
+      }
+
+      const carro = results[0];
+
+      callback(carro);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static save(carro, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `INSERT INTO carro WHERE id = ?`;
+
+    const query = connection.query(sql, carro, (error, results, fields) => {
+      if (error) throw error;
+
+      carro.id = results.insertId;
+
+      callback(carro);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static update(carro, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `UPDATE carro SET ? WHERE id = ?`;
+
+    const id = carro.id;
+
+    const query = connection.query(sql, [carro.id], (error, results, fields) => {
+      if (error) throw error;
+
+      carro.id = results.insertId;
+
+      callback(carro);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static delete(carro, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `DELETE FROM carro WHERE id = ?`;
+
+    const id = carro.id;
+
+    const query = connection.query(sql, id, (error, results, fields) => {
+      if (error) throw error;
+
+      carro.id = results.insertId;
+
+      callback(carro);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+
+  static deleteById(id, callback) {
+    const connection = CarroDB.connect();
+
+    const sql = `DELETE FROM carro WHERE id = ?`;
+
+    const query = connection.query(sql, id, (error, results, fields) => {
+      if (error) throw error;
+
+      carro.id = results.insertId;
+
+      callback(results.affectedRows);
+    });
+
+    console.log(query.sql);
+
+    connection.end();
+  }
+}
+
+module.exports = CarroDB;
---
+++ crud/routes/sample_data.js
@@ -0,0 +1,118 @@
+
+var express = require('express');
+
+var router = express.Router();
+
+var database = require('../database');
+
+router.get("/", function(request, response, next){
+
+	var query = "SELECT * FROM sample_data ORDER BY id DESC";
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('sample_data', {title:'Node.js MySQL CRUD Application', action:'list', sampleData:data});
+		}
+
+	});
+
+});
+
+router.get("/add", function(request, response, next){
+
+	response.render("sample_data", {title:'Insert Data into MySQL', action:'add'});
+
+});
+
+router.post("/add_sample_data", function(request, response, next){
+
+	var first_name = request.body.first_name;
+
+	var last_name = request.body.last_name;
+
+	var age = request.body.age;
+
+	var gender = request.body.gender;
+
+	var query = `
+	INSERT INTO sample_data 
+	(first_name, last_name, age, gender) 
+	VALUES ("${first_name}", "${last_name}", "${age}", "${gender}")
+	`;
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error;
+		}	
+		else
+		{
+			response.redirect("/sample_data");
+		}
+
+	});
+
+});
+
+router.get('/edit/:id', function(request, response, next) {
+  const id = request.params.id;
+  
+  const query = `SELECT * FROM sample_data WHERE id = ${id}`;
+
+  database.query(query, function(error, data) {
+    if(error) throw error;
+    
+    const sample_data = data[0];
+
+    response.render("sample_data",  
+      {
+        title: 'Edit Data into MyQL', 
+        action: 'edit',
+        sample_data: sample_data,
+      });
+  });
+});
+
+router.post('/edit_sample_data/:id', function(request, response, next) {
+  const { first_name, last_name, age, gender } = request.body;
+  const id = request.params.id;
+
+  const query = `
+    UPDATE sample_data SET
+      first_name = '${first_name}',
+      last_name = '${last_name}',
+      age = ${age},
+      gender = '${gender}' 
+    WHERE
+      id = ${id};
+  `;
+
+  database.query(query, function(error, data) {
+    if(error) throw error;
+
+    response.redirect('/sample_data');
+  });
+});
+
+router.get('/delete/:id', function(request, response, next) {
+  const id = request.params.id;
+
+  const query = `
+    DELETE FROM sample_data WHERE id = ${id};
+  `;
+  database.query(query, function(error, data) {
+    if(error) throw error;
+
+    response.redirect('/sample_data');
+  });
+});
+
+module.exports = router;
+
